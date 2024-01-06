00fbe087b55c20a7673694ad3f767f98aca5df55
Vito <hczhch@ymail.com>
2024-01-07 01:14:57 +0800

    /version

--- tensquare_eureka_server/src/main/java/com/tensquare/eureka/constants/FilterOrderConstants.java
+++ tensquare_eureka_server/src/main/java/com/tensquare/eureka/constants/FilterOrderConstants.java
@@ -5,4 +5,7 @@ public class FilterOrderConstants {
     public static final int HEALTH_CHECK_ORDER = 0;
 
     public static final int VERSION_ORDER = 1;
+
+    private FilterOrderConstants() {
+    }
 }
