245d1c5baaa3253d1c81dc62dd67954e68c867c5
Vito <hczhch@ymail.com>
2024-01-07 14:26:55 +0800

    探针

--- tensquare_eureka_server/src/main/java/com/tensquare/eureka/config/WebMvcConfig.java
+++ tensquare_eureka_server/src/main/java/com/tensquare/eureka/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.tensquare.eureka.config;
 
 import com.tensquare.eureka.constants.FilterOrderConstants;
+import com.tensquare.eureka.filter.HealthCheckFilter;
 import com.tensquare.eureka.filter.VersionFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,15 @@ import javax.servlet.DispatcherType;
 
 @Configuration
 public class WebMvcConfig {
+
+    @Bean
+    public FilterRegistrationBean<HealthCheckFilter> healthCheckFilterRegistration(HealthCheckFilter healthCheckFilter) {
+        FilterRegistrationBean<HealthCheckFilter> registrationBean = new FilterRegistrationBean<>(healthCheckFilter);
+        registrationBean.setUrlPatterns(Collections.singletonList("/health-check"));
+        registrationBean.setOrder(FilterOrderConstants.HEALTH_CHECK_ORDER);
+        registrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ERROR);
+        return registrationBean;
+    }
 
     @Bean
     public FilterRegistrationBean<VersionFilter> versionFilterRegistration(VersionFilter versionFilter) {
---
+++ tensquare_eureka_server/src/main/java/com/tensquare/eureka/filter/HealthCheckFilter.java
@@ -0,0 +1,34 @@
+package com.tensquare.eureka.filter;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class HealthCheckFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        String from = request.getParameter("from");
+        //log.trace("===Health Check from {}===", from);
+        System.out.println("===Health Check from {}===" + from);
+        httpServletResponse.getWriter().println("ok");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
